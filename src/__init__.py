19dfe072565526b46c9e7f7ea396e0d7970601e2
unknown <ngkc1996@gmail.com>
2021-09-13 10:29:30 +0800

    edit

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_restful import Api
 # from flask_cors import CORS
-# from src.api import URLShortener
+from src.api import URLShortener
 # from src.api import URLShortener, URLRedirect
+import src.api
 
 def create_app():
 
@@ -12,7 +13,7 @@ def create_app():
 	# CORS(app)
 
 
-	# api.add_resource(URLShortener, '/')
+	api.add_resource(URLShortener, '/')
     # api.add_resource(URLRedirect, '/<id>')
 
 	return app
--- src/api.py
+++ src/api.py
@@ -1,29 +1,29 @@
-# import os
-# from http import HTTPStatus
-# from flask import request, redirect
-# from flask_restful import Resource
+import os
+from http import HTTPStatus
+from flask import request, redirect
+from flask_restful import Resource
 
 # from src.db import add_url, get_url
-
-
 # BASE_URL = os.environ.get('BASE_URL')
 
 
-# class URLShortener(Resource):
-#     def get(self):
-#         return "You called get on /", HTTPStatus.OK
 
 
-    # def post(self):
-    #     data = request.get_json()
-    #     url = data.get('url', None)
-    #     if url is None or url == "":
-    #         return "URL not given.", HTTPStatus.BAD_REQUEST
+class URLShortener(Resource):
+    def get(self):
+        return "You called get on /", HTTPStatus.OK
 
-    #     # db_data = add_url(url)
-    #     # id = db_data.inserted_id
-    #     # return BASE_URL + str(id)
-    #     return "post call seen", HTTPStatus.OK
+
+#     def post(self):
+#         data = request.get_json()
+#         url = data.get('url', None)
+#         if url is None or url == "":
+#             return "URL not given.", HTTPStatus.BAD_REQUEST
+
+#         # db_data = add_url(url)
+#         # id = db_data.inserted_id
+#         # return BASE_URL + str(id)
+#         return "post call seen", HTTPStatus.OK
 
 
 # class URLRedirect(Resource):
