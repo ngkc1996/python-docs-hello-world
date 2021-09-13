73de5a501f4b88d4b5ad8b43ad2a90dcaa4a9460
unknown <ngkc1996@gmail.com>
2021-09-13 09:41:15 +0800

    edit

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-from files import create_app
+from src import create_app
 
 app = create_app()
 # app = Flask(__name__)
 
 # app.config['MONGO_URI'] = os.environ.get('DB_URI')
-
+app.run()
 
 @app.route("/")
 def hello():
