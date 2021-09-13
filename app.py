dd9f037b575c86e8215fbd83ef4e1435a7561f71
unknown <ngkc1996@gmail.com>
2021-09-13 11:45:37 +0800

    edit

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask
 
 # from src import create_app
 from flask_restful import Api
+from flask_cors import CORS
 from src.api import URLShortener, URLRedirect
 
 # app = create_app()
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 
 app.config['MONGO_URI'] = os.environ.get('DB_URI')
