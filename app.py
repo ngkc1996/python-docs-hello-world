0d3994867ffbb38d3bb3b7e7e88505296a53f79e
unknown <ngkc1996@gmail.com>
2021-09-13 11:04:49 +0800

    edit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 # from src import create_app
-from flask_restful import Resource, Api
+from flask_restful import Api
+from src.api import HelloWorld
 
 # app = create_app()
 app = Flask(__name__)
@@ -10,9 +11,7 @@ api = Api(app)
 # app.config['MONGO_URI'] = os.environ.get('DB_URI')
 # app.run()
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+
 
 api.add_resource(HelloWorld, '/')
 
