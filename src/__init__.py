620ea124711a45bd08a6e53432b7fa814af752db
unknown <ngkc1996@gmail.com>
2021-09-13 10:15:35 +0800

    edit

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_restful import Api
 # from flask_cors import CORS
+from src.api import URLShortener
 # from src.api import URLShortener, URLRedirect
-
 
 def create_app():
 
--- src/api.py
+++ src/api.py
@@ -14,27 +14,27 @@ class URLShortener(Resource):
         return "You called get on /", HTTPStatus.OK
 
 
-    def post(self):
-        data = request.get_json()
-        url = data.get('url', None)
-        if url is None or url == "":
-            return "URL not given.", HTTPStatus.BAD_REQUEST
+    # def post(self):
+    #     data = request.get_json()
+    #     url = data.get('url', None)
+    #     if url is None or url == "":
+    #         return "URL not given.", HTTPStatus.BAD_REQUEST
 
-        # db_data = add_url(url)
-        # id = db_data.inserted_id
-        # return BASE_URL + str(id)
-        return "post call seen", HTTPStatus.OK
+    #     # db_data = add_url(url)
+    #     # id = db_data.inserted_id
+    #     # return BASE_URL + str(id)
+    #     return "post call seen", HTTPStatus.OK
 
 
-class URLRedirect(Resource):
-    def get(self, id):
-        # data = get_url(id)
-        # if data:
-        #     url = data['url']
-        #     if not url.startswith(('http://', 'https://')):
-        #         url = 'http://' + url
-        #     return redirect(url, code=HTTPStatus.PERMANENT_REDIRECT)
-        # else:
-        #     return "URL not found.", HTTPStatus.BAD_REQUEST
+# class URLRedirect(Resource):
+#     def get(self, id):
+#         # data = get_url(id)
+#         # if data:
+#         #     url = data['url']
+#         #     if not url.startswith(('http://', 'https://')):
+#         #         url = 'http://' + url
+#         #     return redirect(url, code=HTTPStatus.PERMANENT_REDIRECT)
+#         # else:
+#         #     return "URL not found.", HTTPStatus.BAD_REQUEST
 
-        return "get call seen", HTTPStatus.OK
+#         return "get call seen", HTTPStatus.OK
